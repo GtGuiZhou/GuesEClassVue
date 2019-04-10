cae0f28a600ff9e9b172a530225effe1096c0b07
gtguizhou <gtguizhou@qq.com>
2019-04-10 15:13:54 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,13 @@
 echo '安装包'
 npm i # d2-admin需要用nrm配置npm的源，要不然会出现core-js无法加载的问题
 echo '构建前端工程'
-npx vue-cli-service build --mode production --dest /opt/eclass/GuesEclassTp_v3/public/static/confront --target app --dashboard
+if [ ! -d "/opt/eclass" ];then
+#学校的服务器
+    npx vue-cli-service build --mode production --dest /opt/eclass/GuesEclassTp_v3/public/static/confront --target app --dashboard
+else
+#自己的服务器
+    npx vue-cli-service build --mode production --dest /www/wwwroot/GuesEclassTp/public/static/confront --target app --dashboard
+fi
 # 这种方法也可以
 # ./node_modules/.bin/vue-cli-service build
 
